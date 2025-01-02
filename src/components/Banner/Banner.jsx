8b10eeb03b31a653420c099e7254ec7de1a28a97
Hridoykhan4 <hridoykhan148385@gmail.com>
2025-01-02 20:51:44 +0600

    README updated

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,6 +1,6 @@
 const Banner = () => {
   return (
-    <div className="w-11/12 px-4  mx-auto my-8  bg-[linear-gradient(135deg,rgba(0,0,0,0.1)_20%,rgba(0,0,0,0.4)_50%,transparent_80%),url('./banner-fish.jpg')] bg-blend-overlay  bg-cover bg-right rounded-lg shadow-lg">
+    <div className="w-11/12 px-4  mx-auto my-8  bg-[linear-gradient(135deg,rgba(0,0,0,0.1)_20%,rgba(0,0,0,0.4)_50%,transparent_80%),url('/banner-fish.jpg')] bg-blend-overlay  bg-cover bg-right rounded-lg shadow-lg">
       <div className="md:h-[100vh] justify-center py-4 md:py-0 flex-col space-y-5 text-white flex text-center">
         <h3 className="font-bold text-3xl">Buy Fresh Fishes From FishPalace</h3>
         <p className="font-semibold">We take pride in offering a wide variety of sustainably sourced fish and seafood, <br /> carefully selected to ensure premium quality and exceptional taste</p>
